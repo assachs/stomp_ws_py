46f463cb6c545b1aceff1b35cb797d4e772cd51c
andi <a.sachs@vsd.volley.de>
2025-11-18 23:18:09 +0100

    Logging, Reconnect

--- stomp_ws/client.py
+++ stomp_ws/client.py
@@ -10,8 +10,10 @@ import threading
 
 VERSIONS = '1.0,1.1'
 
+logger = logging.getLogger(__name__)
 
 class Client:
+    kill_now = False
 
     def __init__(self, url, headers={}):
 
@@ -43,11 +45,11 @@ class Client:
         self.hbevent = None
 
     def _on_wsping(self, ws, data):
-        print("WS Ping received and already answered")
+        logger.debug(str(threading.get_ident()) + " WS Ping received and already answered")
 
     def _on_wspong(self, ws, data):
         # Outgoing ping received
-        print("WS Pong recieved on our ping")
+        logger.debug(str(threading.get_ident()) + " WS Pong recieved on our ping")
 
     def _connect(self, timeout=0):
         thread = Thread(target=self.ws.run_forever, kwargs={
@@ -69,30 +71,33 @@ class Client:
 
     def _on_close(self, ws_app, *args):
         self.connected = False
-        logging.debug("Whoops! Lost connection to " + self.ws.url)
+        logger.debug(str(threading.get_ident()) + " Whoops! Lost connection to " + self.ws.url)
         self._clean_up()
 
     def _on_error(self, ws_app, error, *args):
-        print("error")
-        print(error)
+        logger.error(str(threading.get_ident()) + " error")
+        logger.error(error)
         if self._errorCallback is not None:
-            print("error callback")
+            logger.error(str(threading.get_ident()) + " error callback")
             self._errorCallback(error)
         else:
-            print("error callback none")
+            logger.error(str(threading.get_ident()) + " error callback none")
 
     def sendHeartbeat(self):
-        while True:
-            print(str(threading.get_ident()) + " send haeartbeat")
+        while not self.kill_now:
+            logger.info(str(threading.get_ident()) + " send haeartbeat")
             self.ws.send("\n")
             time.sleep(self.hbsendinterval / 1000)
+        logger.info(str(threading.get_ident()) + " send haeartbeat terminated")
 
     def scheduleHeartbeatReceived(self):
+        logger.info(str(threading.get_ident()) + " scheduleHeartbeatReceived")
         thread = Thread(target=self.checkHeartbeatReceived)
         thread.daemon = True
         thread.start()
 
     def checkHeartbeatReceived(self):
+        logger.debug(str(threading.get_ident()) + " checkHeartbeatReceived")
         while self.connected:
             e = threading.Event()
             self.hbevent = e
@@ -101,23 +106,24 @@ class Client:
             if self.hbfirst:
                 waittime = (self.receiveinterval * self.heartbeatgracefirst) / 1000
                 self.hbfirst = False
-            print("waittime" + str(waittime))
+            logger.info("waittime" + str(waittime))
             e.wait(waittime)
             if (time.time() > waittime + self.lastreceived):
-                print(str(threading.get_ident()) + " HB timeout")
+                logger.info(str(threading.get_ident()) + " HB timeout")
                 break
             else:
-                print(str(threading.get_ident()) + " HB ok")
+                logger.info(str(threading.get_ident()) + " HB ok")
 
         if self.connected:
             self.scheduleHeartbeatReceived()
 
     def initHeartbeat(self, frame):
+        logger.debug(str(threading.get_ident()) + " initHeartbeat")
         hb = frame.headers['heart-beat']
         sx, sy = hb.split(",")
 
         self.hbsendinterval = max(int(sx), self.heartbeatsend)
-        print("set hbsendinterval to " + str(self.hbsendinterval))
+        logger.info("set hbsendinterval to " + str(self.hbsendinterval))
 
         thread = Thread(target=self.sendHeartbeat)
         thread.daemon = True
@@ -130,9 +136,9 @@ class Client:
 
 
     def _on_message(self, ws_app, message, *args):
-        print("OnMessage")
-        print(message)
-        logging.debug("\n<<< " + str(message))
+        logger.info(str(threading.get_ident()) + "OnMessage")
+        logger.info(message)
+        logger.debug(str(threading.get_ident()) + "Message Content\n<<< " + str(message))
         _results = []
 
         self.lastreceived = time.time()
@@ -140,7 +146,7 @@ class Client:
             self.hbevent.set()
 
         if str(message) == "\n" :
-            print("stomp haertbeat recieved")
+            logger.info(str(threading.get_ident()) + "stomp haertbeat recieved")
             if self.pingCallback is not None:
                 self.pingCallback()
             return
@@ -149,13 +155,13 @@ class Client:
 
             if frame.command == "CONNECTED":
                 self.connected = True
-                logging.debug("connected to server " + self.url)
+                logger.debug(str(threading.get_ident()) + "connected to server " + self.url)
                 self.initHeartbeat(frame)
 
                 if self._connectCallback is not None:
                     _results.append(self._connectCallback(frame))
             elif frame.command == "MESSAGE":
-
+                logger.info(str(threading.get_ident()) + "message recieved")
                 subscription = frame.headers['subscription']
 
                 if subscription in self.subscriptions:
@@ -178,23 +184,25 @@ class Client:
                     _results.append(onreceive(frame))
                 else:
                     info = "Unhandled received MESSAGE: " + str(frame)
-                    logging.debug(info)
+                    logger.debug(info)
                     _results.append(info)
             elif frame.command == 'RECEIPT':
                 pass
             elif frame.command == 'ERROR':
+                logger.debug(str(threading.get_ident()) + "error recieved")
                 if self._errorCallback is not None:
+                    logger.debug(str(threading.get_ident()) + "error callback")
                     _results.append(self._errorCallback(frame))
             else:
                 info = "Unhandled received MESSAGE: " + frame.command
-                logging.debug(info)
+                logger.debug(str(threading.get_ident()) + " " + info)
                 _results.append(info)
 
         return _results
 
     def _transmit(self, command, headers, body=None):
         out = Frame.marshall(command, headers, body)
-        logging.debug("\n>>> " + out)
+        logger.debug("\n>>> " + out)
         self.ws.send(out)
 
     def connect(self, login=None, passcode=None, headers=None, connectCallback=None, errorCallback=None,
@@ -203,7 +211,7 @@ class Client:
         self._errorCallback = errorCallback
         self.pingCallback = pingCallback
 
-        logging.debug("Opening web socket...")
+        logger.debug(str(threading.get_ident()) + "Opening web socket...")
         self._connect(timeout)
 
         headers = headers if headers is not None else {}
@@ -219,6 +227,8 @@ class Client:
         self._transmit('CONNECT', headers)
 
     def disconnect(self, disconnectCallback=None, headers=None):
+        logger.debug(str(threading.get_ident()) + " >disconnect entering")
+        self.kill_now = True
         if headers is None:
             headers = {}
 
@@ -228,9 +238,12 @@ class Client:
         self._clean_up()
 
         if disconnectCallback is not None:
+            logger.debug(str(threading.get_ident()) + " disconnect callback")
             disconnectCallback()
+        logger.debug(str(threading.get_ident()) + " <disconnect exiting")
 
     def _clean_up(self):
+        logger.debug(str(threading.get_ident()) + " _clean_up")
         self.connected = False
 
     def send(self, destination, headers=None, body=None):
